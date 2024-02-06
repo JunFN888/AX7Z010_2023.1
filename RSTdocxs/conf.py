bdc879a1a07fc3f9db82debb5fbba736fb56bf5e
JunFN888 <junfengwang8888@163.com>
2024-02-06 17:43:54 +0800

    V2.01

---
+++ RSTdocxs/conf.py
@@ -0,0 +1,49 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ZYNQ 7000 development platform FPGA tutorial'
+copyright = '2024 , ALINX '
+author = 'JunFN'
+release = '1.0'
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+
+html_theme_options = {
+    "repository_url": "https://github.com/JunFN888/AX7Z010_2023.1.git",
+    "repository_branch": "master",
+    "home_page_in_toc": True,
+    "use_repository_button": True,
+    "navigation_with_keys": False,
+"extra_footer": """
+        These engineering documents are copyrighted by
+            <a
+                href="https://www.alinx.com/"
+            >ALINX official website</a>
+    """
+}
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'EN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_logo = "src/images/00_media/8.png"
+#highlight_language = 'verilog'
+
---
+++ RSTdocxs/index.rst
@@ -0,0 +1,54 @@
+.. ZYNQ 7000 development platform FPGA tutorial documentation master file, created by
+   sphinx-quickstart on Tue Jan 30 18:14:13 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+.. _ALINX: http://www.alinx.com
+
+
+`ALINX`_ ZYNQ 7000 development platform FPGA tutorial
+=========================================================
+
+.. toctree::
+   :maxdepth: 1
+   :caption: company profile
+
+   src/01_SoftwarePackageDescription_EN
+   src/02_ZYNQIntroduction_EN
+   
+.. toctree::
+   :maxdepth: 2
+   :caption: AX7Z010 model board S1 FPGA tutorial
+
+   src/01_SoftwarePackageDescription_EN
+   src/02_ZYNQIntroduction_EN
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: AX7Z010 model board S2 Vitis tutorial
+   
+   src/01_SoftwarePackageDescription_EN
+   src/02_ZYNQIntroduction_EN
+
+.. toctree::
+   :maxdepth: 2
+   :caption: AX7Z010 model board S3 HLS tutorial
+
+   src/01_SoftwarePackageDescription_EN
+   src/02_ZYNQIntroduction_EN
+
+.. toctree::
+   :maxdepth: 2
+   :caption: AX7Z010 model board S4 LINUX tutorial
+
+   src/01_SoftwarePackageDescription_EN
+   src/02_ZYNQIntroduction_EN
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Copyright Alinx
+
+   src/01_SoftwarePackageDescription_EN
+   src/02_ZYNQIntroduction_EN
+
